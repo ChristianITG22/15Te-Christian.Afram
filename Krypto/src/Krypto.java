d0869d258d21ded6a5e4596a0f20536a2b1aa6e5
ChristianITG22 <christiana9911@outlook.com>
2016-11-16 10:36:09 +0100

    Add files via upload

--- Krypto/src/Krypto.java
+++ Krypto/src/Krypto.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class Krypto {
+	
+	public static void main (String[]args){}
 
-}
+		static int krypto[][] = new int[][]{
+			{1,2,3,3,3},
+			{2,3,4,15,1},
+			{13,4,5,1,2},
+			{14,5,1,2,3},
+			{5,1,2,3,4}};
+			
+			static char key[] = new char[30];
+			static int sizeX=5, sizey=5;
+			
+			Scanner keyb = new Scanner(System.in);
+			
+			
+	}
